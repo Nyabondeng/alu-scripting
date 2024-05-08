425c7759d52ae87383b1950e3bbdcfd386589e33
Nyabondeng <n.adut1@alustudent.com>
2024-05-08 15:48:29 -0700

    1-repetition_token_0.rb

--- regular_expressions/1-repetition_token_0.rb
+++ regular_expressions/1-repetition_token_0.rb
@@ -1,4 +1,4 @@
-[200~#!/usr/bin/env ruby
-  # Match "hbttn, hbtttn, hbttttn, hbtttttn"
-  #
-  # puts ARGV[0].scan(/hbt{2,5}n/).join
+#!/usr/bin/env ruby
+# Match "hbttn, hbtttn, hbttttn, hbtttttn"
+#
+# puts ARGV[0].scan(/hbt{2,5}n/).join
